5a4eaade7e2658ac8349d6877a7d28aa1a01cecc
hookSSi <sounghoo12@naver.com>
2020-09-08 12:05:56 +0900

    기본적인 세팅
    - Window Object 생성
    - Vulkan Object 생성

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Make Game With Vulkan
- Vulkan
+이 저장소는 Vulkan API를 공부하기 위한 저장소입니다.
+
+# 참고하는 자료
+
+* https://vulkan-tutorial.com/Introduction
+* https://vulkan.lunarg.com/doc/sdk/1.2.148.1/windows/getting_started.html
+* https://ljhblog.tistory.com/82
+
+# 개발 환경
+* Visual Studio 2019
+* C++17
+* GLFW
+* GLM
+* SDL
+
+# 무엇을 만들 것인지?
+
+아직 명확하게 정해진 것은 없지만 Vulkan API 개발환경부터 천천히 해볼 생각입니다.
---
+++ main.cpp
@@ -0,0 +1,111 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <SDL.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication
+{
+public:
+	void run()
+	{
+		InitWindow();
+		InitVulkan();
+		MainLoop();
+		Cleanup();
+	}
+private:
+	GLFWwindow* window;
+	VkInstance vkInstance;
+
+	/// GLFW로 윈도우 생성
+	void InitWindow()
+	{
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
+	/// Vulkan 오브젝트들을 초기화
+	void InitVulkan()
+	{
+		CreateInstance();
+	}
+
+	void CreateInstance()
+	{
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+		createInfo.enabledLayerCount = 0;
+
+		VkResult result = vkCreateInstance(&createInfo, nullptr, &vkInstance);
+		if (result != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create instance!");
+		}
+	}
+
+	/// 매 프레임 렌더링
+	void MainLoop()
+	{
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+		}
+	}
+
+	/// 메모리 회수
+	void Cleanup()
+	{
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
+};
+
+int main(int argc, char* argv[]) 
+{
+	HelloTriangleApplication app;
+
+	try
+	{
+		app.run();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
